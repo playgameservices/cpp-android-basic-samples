98d0da4464661e7efdb090327a4d9c201d186739
ggfan <gfan@google.com>
2015-08-31 17:16:39 -0700

    addressing code review issue

--- samples-android/ButtonClicker/jni/Application.mk
+++ samples-android/ButtonClicker/jni/Application.mk
@@ -1,7 +1,8 @@
-APP_PLATFORM := android-22
+APP_PLATFORM := android-9
+APP_ABI := armeabi-v7a
+
+#APP_ABI:= arm64-v8a
 #APP_ABI := all
-#APP_ABI := armeabi-v7a
-APP_STL := arm64-v8a
 
 APP_STL := c++_static
 
